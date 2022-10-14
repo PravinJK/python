1e8c65542ad5787bd028c47d38b33ea0fa4ce5e3
PravinJK <34760247+PravinJK@users.noreply.github.com>
2022-10-15 00:52:57 +0530

    Add files via upload

---
+++ wordgame.py
@@ -0,0 +1,59 @@
+import random
+words = ['game','lion','tiger','coder','techie','google','football','friends','university','hunter']
+word = random.choice(words)
+maskedword =[]
+length = len(word)
+while length>0:
+    maskedword.append('*')
+    length-=1
+mask = len(word)*'*'   
+wrongguess = len(word)
+print("Word is ", mask , "its length is ", len(word))
+turns = 0
+index = 0
+previous =''
+correct =''
+while(turns<wrongguess):
+    print("\nyou have ", (wrongguess - turns), "attempts left")
+    print("Enter a char(lowercase) : ", end='')
+    guess=input()
+    if(guess not in word):
+        if(guess not in previous):
+            turns+=1
+        previous+=guess
+        print("Previous guess : ", previous)
+        if(turns<wrongguess):
+             print("Try again...")
+             print("word is ", end='')
+             for i in maskedword:
+                 print (i, end='')
+    else:
+        if(guess not in correct):
+            
+            print("correct..")
+            index = 0
+            for i in word:
+                if(word[index]==guess):
+                    maskedword[index]=guess
+                index=index+1;
+            if(turns<wrongguess):
+                print("word is ", end='')
+                for i in maskedword:
+                    print (i, end='')
+            correct+=guess
+        else:
+            print("you have already guessed this character")
+            print("Try again...")
+            print("word is ", end='')
+            for i in maskedword:
+                print (i, end='')
+            
+    if('*' not in maskedword):
+        print("\nCongratulations you got the word")
+        break;
+if(turns==wrongguess):
+    print("word is ", word)
+    print("you lost")
+                
+        
+        
